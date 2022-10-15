5bedf06a2949c3ed4dc564b3ea83e47f472e0833
Zach Ruiz <powersbooth10@gmail.com>
2022-10-14 21:48:55 -0500

    commit tree a0cc29009b8aff3a2a9e031553aeefe4ff360e03

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
+require 'pry'
+
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+    array_of_names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(array_of_names)
+        array_of_names.map.with_index(1) do |name, room|
+            "Hello, #{name}! You'll be assigned to room #{room}!"
+        end
+end
+
+def printer(array_of_names)
+    batch_badge_creator(array_of_names).each do |badge|
+        puts badge
+    end
+    assign_rooms(array_of_names).each do |room|
+        puts room
+    end
+end
+# binding.pry
